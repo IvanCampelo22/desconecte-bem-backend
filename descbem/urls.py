a9efc80d6081c3ecaa9a6c2f79b3fd1384124d53
ivan <ivancampelo1973@gmail.com>
2024-10-08 09:56:24 -0300

    feat: outras pequenas alterações

--- descbem/urls.py
+++ descbem/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from user import views as user
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from rest_framework import routers
+from notification import views as notification
+from rest_framework import routers
 
-# router = routers.DefaultRouter()
-# router.register(r'image', user.ImageUploadViewSet, basename='image')
+router = routers.DefaultRouter()
+router.register(r'image', user.ImageUploadViewSet, basename='image')
 
 urlpatterns = [
-    # path(r'api/', include(router.urls)),
+    path(r'api/', include(router.urls)),
     path('admin/', admin.site.urls),
 
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -36,4 +37,7 @@ urlpatterns = [
     path('change_password/<int:pk>/', user.ChangePasswordView.as_view(), name='auth-change-password'),
     path('password_reset/', user.PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('password_reset/<str:uidb64>/<str:token>/', user.PasswordResetView.as_view(), name='password-reset-confirm'),
+
+    path('notifications/', notification.NotificationView.as_view(), name='notifications'),
+    path('notifications/<int:pk>/', notification.NotificationDetailView.as_view(), name="notifications-detail"),
 ]
